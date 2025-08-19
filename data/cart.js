436bdf537df720f74292cd29395593b745427f4b
PrabalKumar2311 <prabalhayden747@gmail.com>
2025-08-19 12:37:53 +0530

    localStorage added, clicking on delete updates HTML on page

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,5 @@
-export let cart = [
-  
-  {
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },
@@ -8,8 +7,11 @@ export let cart = [
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }
-
 ];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
   let matchingItem;
@@ -33,18 +35,23 @@ export function addToCart(productId){
     }
 
     console.log(cart);
+
+    saveToStorage();
 }
 
 export function deleteFromCart(productId){
   const newCart = [];
 
   cart.forEach((cartItem) => {
-    if(cartItem.productId === productId){
+    if(cartItem.productId !== productId){
       newCart.push(cartItem);
     }
 
     cart = newCart;
+    saveToStorage();
   })
+
+
 }
 
 
